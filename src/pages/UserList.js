d27fa82fca55ae1fc61a8205d694f42c9a779288
Otmane-dev <83417450+Otmane-dev@users.noreply.github.com>
2022-02-11 14:24:24 +0100

    ajouté coup de coeur

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect} from 'react';
+import { useState } from "react/cjs/react.development";
+import Card from '../components/Card';
 import Header from '../components/Header';
 
 const UserList = () => {
+    const [listData, setListData] =useState([]);
+
+    useEffect(() => {
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(",") : [];
+        
+        for (let i = 0; i < moviesId.length; i++ ) {
+
+        
+
+        axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`).then((res) => setListData((listData) => [...listData, res.data]));
+    }
+    }, [])
+
     return (
-        <div>
+        <div className="user-list-page">
             <Header />
+            <h2>Coup de coeur <span>❤️</span></h2>
+            <div className="result">
+        {listData.length > 0 ? (listData.map((movie) => <Card movie={movie} key={movie.id}/>)) : (<h2>Aucun coup de coeur</h2>)}
+            </div>
         </div>
     );
 };
-
+//`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
 export default UserList;
